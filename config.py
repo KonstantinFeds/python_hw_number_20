138d4ba65555d581cd2878b93a6b6ce1bfa9a1ff
konstatin fedoseev <kons.fed.99@gmail.com>
2025-12-04 19:13:43 +0300

    update tests

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-
 import os
 from dotenv import load_dotenv
 from appium.options.android import UiAutomator2Options
 import utils.file
+
 
 
 def to_driver_options(context):
@@ -12,12 +12,8 @@ def to_driver_options(context):
         options.set_capability('remote_url', os.getenv('REMOTE_URL'))
         options.set_capability('deviceName', os.getenv('DEVICE_NAME'))
         options.set_capability('appWaitActivity', os.getenv('APP_WAIT_ACTIVITY'))
-        options.set_capability('appActivity', 'org.wikipedia.main.MainActivity')
-        options.set_capability('autoLaunch', True)
-        options.set_capability('autoGrantPermissions', True)
         options.set_capability('app', utils.file.abs_path_from_project(os.getenv('APP')))
-        options.set_capability('appPackage',os.getenv('APP_PACKAGE'))
-        options.set_capability('platformName',os.getenv('PLATFORM_NAME'))
+
 
     if context == 'bstack':
         options.set_capability('remote_url', os.getenv('REMOTE_URL'))
@@ -39,38 +35,3 @@ def to_driver_options(context):
         )
 
     return options
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-"""
-#код с урока
-
-#context = os.getenv('context', 'bstack')
-#run_on_bstack = os.getenv('run_on_bstack','false').lower() == 'true'
-remote_url = os.getenv('remote_url','http://127.0.0.1:4723')
-deviceName = os.getenv('deviceName')
-appActivity = os.getenv('appActivity','org.wikipedia.main.MainActivity')
-app = os.getenv('app','./app-alpha-universal-release.apk')
-runs_on_bstack = app.startswith('bs://')
-appPackage = os.getenv('appPackage','org.wikipedia.alpha')
-if runs_on_bstack:
-    remote_url = 'http://hub.browserstack.com/wd/hub'
-USER_NAME_BSTACK = os.getenv('bstack_userName', 'bsuser_SVwepu')
-ACCESS_KEY_BSTACK = os.getenv('bstack_accessKey', 'RbgXQs1ipEuaWioynt6U')
-
-"""
--- utils/file.py
+++ utils/file.py
@@ -1,9 +1,10 @@
+import utils
+from pathlib import Path
+
 def abs_path_from_project(relative_path: str):
-    import tests
-    from pathlib import Path
 
     return (
-        Path(tests.__file__)
+        Path(utils.__file__)
         .parent.parent.joinpath(relative_path)
         .absolute()
         .__str__()
